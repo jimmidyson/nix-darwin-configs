3fffeacb1c419bb77842f351f05611d3272ae0a0
Jimmi Dyson <jimmidyson@gmail.com>
2023-12-12 10:07:32 +0000

    fix: Support latest gh

--- home-manager/programs.nix
+++ home-manager/programs.nix
@@ -21,6 +21,10 @@
 
     gh = {
       enable = true;
+      settings = {
+        # Workaround for https://github.com/nix-community/home-manager/issues/4744
+        version = 1;
+      };
     };
 
     go = {
